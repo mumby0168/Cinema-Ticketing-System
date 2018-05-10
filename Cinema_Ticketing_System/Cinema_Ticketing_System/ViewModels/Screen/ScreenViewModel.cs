6dd0d7c55e3d9222c19e851f58b67de544a1cf94
Robert Bennett <robertbennett1998@outlook.com>
2018-05-10 08:25:21 +0100

    Ready for database changes

---
+++ Cinema_Ticketing_System/Cinema_Ticketing_System/ViewModels/Screen/ScreenViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Ticketing_System.ViewModels.Screen
+{
+    class ScreenViewModel : IDisposable
+    {
+        private DatabaseContext m_Context = new DatabaseContext();
+        void IDisposable.Dispose()
+        {
+            DatabaseContext.Dispose();
+        }
+
+        public ScreenViewModel()
+        {
+
+        }
+    }
+}
