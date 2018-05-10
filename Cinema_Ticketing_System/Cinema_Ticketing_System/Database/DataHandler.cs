53a90a71d89f1e9ed1ed9cb51b80f85d32bc25a2
Robert Bennett <robertbennett1998@outlook.com>
2018-05-10 09:11:09 +0100

    Nearly ready to dev

---
+++ Cinema_Ticketing_System/Cinema_Ticketing_System/Database/DataHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Ticketing_System.Database
+{
+    class DataHandler : IDisposable
+    {
+        private CinemaContext m_DatabaseContext;
+        void IDisposable.Dispose()
+        {
+            m_DatabaseContext.SaveChangesAsync();
+            m_DatabaseContext.Dispose();
+        }
+
+        public DataHandler()
+        {
+            m_DatabaseContext = new CinemaContext();
+        }
+    }
+}
--- Cinema_Ticketing_System/Cinema_Ticketing_System/Migrations/Configuration.cs
+++ Cinema_Ticketing_System/Cinema_Ticketing_System/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace Cinema_Ticketing_System.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Cinema_Ticketing_System.CinemaContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Cinema_Ticketing_System.Database.CinemaContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Cinema_Ticketing_System.CinemaContext context)
+        protected override void Seed(Cinema_Ticketing_System.Database.CinemaContext context)
         {
             //  This method will be called after migrating to the latest version.
 
