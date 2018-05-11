a0b55fd07adfaed36a6006285497a5ccdba3aadf
billy mumby <billy.mumby@outlook.com>
2018-05-11 11:20:44 +0100

    added navigation

--- Cinema_Ticketing_System/Cinema_Ticketing_System/ViewModels/ShellViewModel.cs
+++ Cinema_Ticketing_System/Cinema_Ticketing_System/ViewModels/ShellViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cinema_Ticketing_System.ViewModels.Commands;
 using Cinema_Ticketing_System.ViewModels.Screen;
 
 namespace Cinema_Ticketing_System.ViewModels
@@ -30,10 +31,15 @@ namespace Cinema_Ticketing_System.ViewModels
         private ChartLandingPageViewModel chartLandingPageViewModel;
 
 
+        public ClickCommand goToBook { get; private set; }
 
+        public ClickCommand GoToCharts { get; private set; }
 
         public ShellViewModel()
         {            
+
+            goToBook = new ClickCommand(BookTicketClicked);
+            GoToCharts = new ClickCommand(ViewCharts);
             screenViewModel = new ScreenViewModel();
 
             InitialTicketBookingFormViewModel = new InitialTicketBookingFormViewModel((screeningId, tickets) =>
