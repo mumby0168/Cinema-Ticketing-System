bb849e745fae8fb03cdd08a68f760bf58309fd45
billy mumby <billy.mumby@outlook.com>
2018-06-07 13:12:03 +0100

    Added the pdf and the code to open it.

--- Cinema_Ticketing_System/Cinema_Ticketing_System/ViewModels/HomePageViewModel.cs
+++ Cinema_Ticketing_System/Cinema_Ticketing_System/ViewModels/HomePageViewModel.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cinema_Ticketing_System.ViewModels.Commands;
 
 namespace Cinema_Ticketing_System.ViewModels
 {
     public class HomePageViewModel
     {
+        public ClickCommand OpenPdfClick { get; private set; }
+
+        public HomePageViewModel()
+        {
+            OpenPdfClick = new ClickCommand(OpenPdf);
+        }
+
+        public void OpenPdf()
+        {
+            string dir = AppDomain.CurrentDomain.BaseDirectory + "../../../../UG.pdf";
+            Process.Start(dir);
+        }
     }
 }
